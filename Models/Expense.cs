3c638b5ed4c2f52a9577456dfaa89b0a503d031b
Arina <arinapereteatcu0@gmail.com>
2025-05-16 21:58:46 +0300

    Add Expense model

---
+++ Models/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Expense_WEB.Models;
+
+public class Expense
+{
+    [Key]
+    public int Id { get; set; }
+        
+    [Required]
+    public string Title { get; set; }
+        
+    [Required]
+    public decimal Amount { get; set; }
+        
+    public DateTime Date { get; set; }
+        
+    public string Category { get; set; }
+        
+    public string Notes { get; set; }
+        
+    public string UserId { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace Expense_WEB.Models;
+
+public class User
+{
+    
+}
