95f4af78d4bf043c8a3991c842e18bef98a528c1
Arina <arinapereteatcu0@gmail.com>
2025-05-28 23:27:40 +0300

    Add token controller

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,35 @@
+﻿using Expense_WEB.DTOs;
+
+namespace Expense_WEB.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+
+        public TokenController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+        
+        [HttpPost]
+        public IActionResult GenerateToken([FromBody] TokenDtos.TokenRequest request)
+        {
+            try
+            {
+                var response = _tokenService.GenerateToken(request);
+                return Ok(response);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
+    }
+
+}
+
